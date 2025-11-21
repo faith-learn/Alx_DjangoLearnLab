cdb5307caec3b30de6147cb2fc45820c32457f75
Faith Awuor <faithhope264@gmail.com>
2025-11-21 06:26:47 +0300

    Building Your First API Endpoint with Django REST Framework

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,10 +1,6 @@
-from rest_framework import routers
-from django.urls import path, include
-from .views import BookViewSet
-
-router = routers.DefaultRouter()
-router.register(r'books', BookViewSet, basename='book')
+from django.urls import path
+from .views import BookList
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('books/', BookList.as_view(), name='book-list'),
 ]
