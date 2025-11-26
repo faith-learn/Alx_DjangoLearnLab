acae23de9c904ab08c20cb3224a78636f1c111e6
Faith Awuor <faithhope264@gmail.com>
2025-11-26 18:21:32 +0300

    Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/advanced_api_project/urls.py
+++ advanced-api-project/advanced_api_project/urls.py
@@ -1,42 +1,7 @@
-"""
-URL configuration for advanced_api_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-from django.urls import path
-from .views import (
-    BookListView,
-    BookDetailView,
-    BookCreateView,
-    BookUpdateView,
-    BookDeleteView
-)
+from django.contrib import admin
+from django.urls import path, include  # must import include
 
 urlpatterns = [
-    # List all books
-    path('books/', BookListView.as_view(), name='book-list'),
-    
-    # Retrieve a single book by ID
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    
-    # Create a new book
-    path('books/create/', BookCreateView.as_view(), name='book-create'),
-    
-    # Update an existing book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    
-    # Delete a book
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),  # include your app-level URLs
 ]
