f5990ebf221151f9552fb1999f8c1ad56c449002
Faith Awuor <faithhope264@gmail.com>
2025-12-07 14:27:28 +0300

    Delete old blog and django_blog folders to recreate clean structure

--- blog/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'published_date')
-    search_fields = ('title', 'content')
-    list_filter = ('published_date', 'author')
