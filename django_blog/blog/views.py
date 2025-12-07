8c6194fdc11bf51ea6e6f7042309b0f51d72ec96
Faith Awuor <faithhope264@gmail.com>
2025-12-07 19:33:35 +0300

    Implementing Advanced Features: Tagging and Search Functionality

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,87 +1,15 @@
-from django.contrib.auth.decorators import login_required
-from django.views.generic import ListView, DetailView, DeleteView, CreateView, UpdateView
-from django.urls import reverse_lazy
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib import messages
-from django.contrib.auth import login
+from django.shortcuts import render
 from django.db.models import Q
+from .models import Post
 
-from .forms import PostForm, RegisterForm, UpdateUserForm, CommentForm
-from .models import Post, Comment
-
-
-# ================================
-# USER REGISTRATION
-# ================================
-def register(request):
-    if request.method == "POST":
-        form = RegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, "Account created successfully!")
-            return redirect("profile")
-    else:
-        form = RegisterForm()
-    return render(request, "register.html", {"form": form})
-
-
-# ================================
-# USER PROFILE
-# ================================
-@login_required
-def profile(request):
-    if request.method == "POST":
-        form = UpdateUserForm(request.POST, instance=request.user)
-        if form.is_valid():
-            form.save()
-            messages.success(request, "Profile updated successfully!")
-            return redirect("profile")
-    else:
-        form = UpdateUserForm(instance=request.user)
-
-    return render(request, "profile.html", {"form": form})
-
-
-# ================================
-# COMMENT CRUD
-# ================================
-class CommentCreateView(LoginRequiredMixin, CreateView):
-    model = Comment
-    form_class = CommentForm
-    template_name = "comments/add_comment.html"
-
-    def form_valid(self, form):
-        post = get_object_or_404(Post, pk=self.kwargs['post_id'])
-        form.instance.post = post
-        form.instance.author = self.request.user
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.kwargs['post_id']})
-
-
-class CommentUpdateView(LoginRequiredMixin, UpdateView):
-    model = Comment
-    form_class = CommentForm
-    template_name = 'comments/edit_comment.html'
-
-    def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.object.post_id})
-
-    def dispatch(self, request, *args, **kwargs):
-        comment = self.get_object()
-        if comment.author != request.user:
-            return redirect('post_detail', pk=comment.post.id)
-        return super().dispatch(request, *args, **kwargs)
-
-
-class CommentDeleteView(LoginRequiredMixin, DeleteView):
-    model = Comment
-    template_name = 'comments/delete_comment.html'
-
-    def get_success_url(self):
-        return reverse_lazy('post_detail', kwargs={'pk': self.object.post_id})
-
+def search_posts(request):
+    query = request.GET.get('q')
+    results = []
     
+    if query:
+        results = Post.objects.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(tags__name__icontains=query)
+        ).distinct()
+    return render(request, 'blog/search_results.html', {'results': results, 'query': query})
