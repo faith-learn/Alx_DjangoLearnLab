a1307679036b0e3891fbc95a4b85eae91cb928bc
Faith Awuor <faithhope264@gmail.com>
2025-12-07 17:07:54 +0300

    Delete django_blog/django_blog directory

--- django_blog/django_blog/blog/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Post
-
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'status', 'published_date')
-    list_filter = ('status', 'author')
-    search_fields = ('title', 'content')
-    ordering = ('-published_date',)
